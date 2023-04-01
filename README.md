ae6ba859dcb614523a53d35f37787ac148312689
LueAdm <lueadm@gluebin.com>
2023-04-01 11:54:20 +0700

    adding http module interface

---
+++ README.md
@@ -0,0 +1,15 @@
+# Glue SDK
+
+Glue software development kit for Golang, which gives interaction contracts and helps to glue stable modules into one solid service.
+
+* About glue bin platform [https://gluebin.com/platform](https://gluebin.com/platform)
+* The platform license scheme [https://gluebin.com/license-scheme](https://gluebin.com/license-scheme)
+* Glued service templates [https://gluebin.com/license-scheme](https://gluebin.com/market-place/templates)
+
+## Getting Started
+
+* Glue container
+* Module configuration
+* Module interaction
+* Module deployment and release
+* Publish your modules to the Market Place
---
+++ sdk.go
@@ -0,0 +1,70 @@
+package glue
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/spf13/viper"
+	"gorm.io/gorm"
+)
+
+type Container interface {
+	Get(id string) (component interface{}, found bool)
+	Set(id string, component interface{}, overwrite bool) (actual interface{}, existed bool)
+}
+
+type Glue interface {
+	Container() Container
+	Http() Http
+	Sql() Sql
+	Config() *viper.Viper
+}
+
+type Error struct {
+	Code        string `json:"code"`
+	Description string `json:"description"`
+}
+
+func (e *Error) Error() string {
+	return e.Code
+}
+
+var RequestMalformError = &Error{
+	Code: "request_malform",
+}
+
+var UnknownError = &Error{
+	Code:        "unknown_error",
+	Description: "The module encounter an unknown error",
+}
+
+var NotFoundError = &Error{
+	Code:        "not_found",
+	Description: "There is no record found",
+}
+
+type Dto interface {
+	Validate() error
+}
+
+type Http interface {
+	JwtSign(c *gin.Context, data map[string]interface{}, expiresAt int64) (actual map[string]interface{}, token string, err error)
+	JwtValidate(c *gin.Context, token string) (map[string]interface{}, bool)
+	NewSessionCookie() *http.Cookie
+	AddIdentityProvider(provider func(data map[string]interface{}))
+	Router() *gin.RouterGroup
+	Identity(*gin.Context) map[string]interface{}
+	Authorizer() gin.HandlerFunc
+	Authorized(func(*gin.Context)) func(*gin.Context)
+	BindAndValidate(c *gin.Context, dto Dto) (valid bool)
+	SendJson(c *gin.Context, data interface{}, err error)
+}
+
+type Sql interface {
+	DB(databaseId ...string) (db *gorm.DB)
+}
+
+type Module interface {
+	Plug(Glue)
+	Run()
+}
